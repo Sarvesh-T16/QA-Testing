a8aea5cbd8453230d99ec26938a09fea20eeebc0
immanuel-rajadurai <91907788+immanuel-rajadurai@users.noreply.github.com>
2024-10-30 12:15:50 +0000

    Create TestBlackJack.java

---
+++ QA-Testing/src/test/java/TestBlackJack.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBlackJack {
+
+    @Test
+    public void testBlackJack() {
+        Blackjack blackjack = new Blackjack();
+
+        int val1 = blackjack.calculateBlackjack(20,10);
+        int val2 = blackjack.calculateBlackjack(22,30);
+        int val3 = blackjack.calculateBlackjack(22,20);
+
+        assertEquals(val1, 20);
+        assertEquals(val2, 0);
+        assertEquals(val3, 20);
+
+
+    }
+}
